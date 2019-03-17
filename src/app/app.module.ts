b8eb7b4477f29359121d68b47fe386ce62f007da
Peter <petermat402@gmail.com>
2019-03-17 19:58:49 +0100

    some initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EntryComponent } from './entry/entry.component';
+import {RouterModule} from '@angular/router';
+import {routes} from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
